e9ca6ec7ecca1a8cdc10f4e98afdcc56aba5a18c
Roushan Kumar <roushankumarcool@gmail.com>
2023-07-03 13:30:18 +0530

    check

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const jsonServer = require('json-server')
-const cors= require("cors")
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(cors())
+
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT || 3000, () => {
+
+const PORT=4500
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
